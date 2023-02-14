bbf98b5c59fd3a5929c23c5bc4d292b3d4cc0c66
Trevo <Trevo@DESKTOP-DCC7DMS>
2023-02-14 15:25:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Methods_Deliverable
+{
+    class Program
+    {
+        static int[] random_array(int array_len)
+        {
+            Random random = new Random();
+            int[] MyArray = new int[array_len];
+            for (int i = 0; i < array_len; i++)
+            {
+                MyArray[i] = random.Next(10, 50);
+            }
+            return MyArray;
+                }
+        static int sum(int[] array)
+        {
+            int sum = 0;
+            foreach(int item in array)
+            {
+                sum = sum + item;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer between 5 and 15");
+            int input = int.Parse(Console.ReadLine());
+            int[] sample_Array = random_array(input);
+            
+            Console.WriteLine("The elements in the random array are: ");
+            foreach (int element in sample_Array)
+            {
+                Console.WriteLine(element + " ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("The sum is: " + sum(sample_Array));
+        }
+    }
+}
